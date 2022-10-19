06590bf06bb1e62c57a00a415954aab02a75a33e
Samuel <samuel.aoliveira74@gmail.com>
2022-10-19 17:44:53 -0300

    feat: added many modules to PeopleModule | made forms in AddPersonComponent | added a button to open dialog in ListPeopleComponent

--- src/app/people/people.module.ts
+++ src/app/people/people.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
+import { MatInputModule } from '@angular/material/input';
 import { PeopleRoutingModule } from './people-routing.module';
 import { MatTableModule } from '@angular/material/table';
 import { PeopleComponent } from './people.component';
 import { ListComponent } from './list/list.component';
-
+import { AddPersonComponent } from './add-person/add-person.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @NgModule({
-  declarations: [PeopleComponent, ListComponent],
-  imports: [CommonModule, PeopleRoutingModule, MatTableModule, MatButtonModule],
+  declarations: [PeopleComponent, ListComponent, AddPersonComponent],
+  imports: [
+    CommonModule,
+    PeopleRoutingModule,
+    MatTableModule,
+    MatButtonModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
 export class PeopleModule {}
