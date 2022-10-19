d0640846500f791deb85c1142cd9d918442c42d9
Samuel <samuel.aoliveira74@gmail.com>
2022-10-19 14:38:59 -0300

    feat: added PeopleModule to AppModule imports | added to AppModuleRouting a route in lazy-loading way to People context | added <router-outlet> to the HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeopleModule } from './people/people.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, PeopleModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
